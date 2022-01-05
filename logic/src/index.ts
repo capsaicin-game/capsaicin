0ac91d95075c93ef6870aa65481212d4ca491532
Riley Stewart <me@rileystew.art>
2022-01-04 17:18:52 -0800

    this is a typescript repo, partner

--- logic/src/index.ts
+++ logic/src/index.ts
@@ -1,48 +1,148 @@
-enum Pepper {
-  Red,
-  Blue,
-  Yellow,
-  Green,
-  Orange,
-  Purple,
-  Brown,
-  White,
-  Black,
-  Ghost
+type PepperColor = "Red"|"Blue"|"Yellow"|"Green"|"Orange"|"Purple"|"Brown"|"White"|"Black"|"Ghost";
+type PlayerColor = "Red"|"Blue"|"Yellow"|"Green"|"Orange"|"Purple";
+type Role = "Path"|"Harvest"|"Plant";
+type BonusAction = "ExtraPlant"|"ExtraMove"|"TurnAround";
+
+function isEven(n: number): boolean {
+  return n % 2 == 0;
 }
 
-enum Player {
-  Red,
-  Blue,
-  Yellow,
-  Green,
-  Orange,
-  Purple
+function absDiff(x: number, y: number): number {
+  return Math.abs(x - y);
 }
 
-class Point {
+interface Pepper {
+  kind: 'pepper',
+  color: PepperColor;
+}
+
+interface Player {
+  kind: 'player',
+  color: PlayerColor;
+}
+
+/*
+ * A point on the game grid.  There are three kinds of points - path, harvest, and plant
+ */
+interface Point {
   x: number;
   y: number;
+}
 
-  constructor(x: number, y: number) {
-    this.x = x;
-    this.y = y;
+function pointEq(a: Point, b: Point) {
+  return a.x === b.x && a.y === b.y;
+}
+
+function pointRole(point: Point): Role {
+  if (isEven(point.x) && isEven(point.y)) {
+    return 'Path';
+  } else if (isEven(point.x) || isEven(point.y)) {
+    return 'Harvest';
+  } else {
+    return 'Plant';
   }
 }
 
-class PepperPatch {
-  // TODO: use fp-ts Optional?
-  peppers: Pepper|null[];
-  farmers: Player|null[];
+function pointString(point: Point) {
+  return `${pointRole(point)}: ${point.x}, ${point.y}`;
+}
 
-
-  constructor() {
-    this.peppers = [];
-    this.farmers = [];
+/*
+  * Find the path point between two harvest points
+  * @throws
+  */
+function pathBetween(point: Point, other: Point): Point|null {
+  if (pointRole(point) !== 'Harvest' || pointRole(other) !== 'Harvest') {
+    throw new Error(`Both points need to be harvest points: ${pointString(point)} ${pointString(other)}`);
+  } else if (absDiff(point.x, other.x) > 1 || absDiff(point.y, other.y) > 1) {
+    return null;
+  }
+  if (isEven(point.x) && isEven(other.y)) {
+    return { x: this.x, y: other.y };
+  } else if (isEven(this.y) && isEven(this.x)) {
+    return { x: other.x, y: this.y };
+  } else {
+    return null;
   }
 }
 
-class ScovilleGame {
-  players: Player[];
-  patch: PepperPatch;
+type PatchItem = Pepper|Player|null;
+
+
+
+interface PepperPatch {
+  grid: PatchItem[];
+  w: number;
+  h: number;
+  players: { [ key: PlayerColor ]: Point };
 }
+
+interface HarvestMove {
+  path: Point[];
+  player: Player;
+  bonus_actions: BonusAction[];
+}
+
+function createPepperPatch(w = 20, h = 14) {
+  let grid = [];
+  for (let i = 0; i < w * h; i++) {
+    grid.push(null);
+  }
+}
+
+function pointToGrid(patch: PepperPatch, point: Point) {
+  return point.x + point.y * patch.w;
+}
+
+function getPepper(patch: PepperPatch, point: Point) {
+  if (pointRole(point) !== 'Plant') {
+    throw new Error(`getPepper called on a non-planting point ${pointString(point)}`)
+  }
+  return patch.grid[pointToGrid(patch, point)];
+}
+
+function getPlayer(patch: PepperPatch, point: Point) {
+  if (pointRole(point) !== 'Harvest') {
+    throw new Error(`getPlayer called on a non-harvest point ${pointString(point)}`)
+  }
+  return patch.grid[pointToGrid(patch, point)];
+}
+
+function maxMoveLength(move: HarvestMove) {
+  if (move.bonus_actions.includes('ExtraMove')) {
+    return 4;
+  } else {
+    return 3;
+  }
+}
+function validHarvest(patch: PepperPatch, move: HarvestMove) {
+  if (move.path.length > maxMoveLength(move)) {
+    throw new Error(`Harvest path too long!`);
+  } else if (!patch.players[move.player.color]) {
+    throw new Error(`Invalid player ${move.player.color}`);
+  }
+  let point = patch.players[move.player.color];
+  if (!pointEq(point, move.path[0])) {
+    throw new Error('Player not on starting point of move');
+  }
+  let visited = [];
+  for (let next of move.path.slice(1)) {
+    let pathPart = pathBetween(point, next);
+    if (!pathPart) {
+      throw new Error(`No path from ${pointString(point)} to ${pointString(next)}`);
+    }
+    if (visited.find(p => pointEq(pathPart, p))) {
+      if (move.bonus_actions.includes("TurnAround")) {
+        move.bonus_actions = move.bonus_actions.filter(ba => ba !== "TurnAround");
+      } else {
+        throw new Error(`Cannot double back at ${pointString(point)}`);
+      }
+    }
+    let blocker = getPlayer(patch, next);
+    if (blocker) {
+      throw new Error(`Player ${blocker.color} is blocking the way`);
+    }
+    point = next;
+  }
+}
+
