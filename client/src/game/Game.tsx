73b6bd969d376ba28323e2cebef4e701bd6920b8
Riley Stewart <me@rileystew.art>
2022-02-08 22:40:08 -0800

    Patchwork, css

--- client/src/game/Game.tsx
+++ client/src/game/Game.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import Patch from '../components/Patch';
 
 export const Game = (): React.ReactElement => {
   return (
     <div className="board">
       <div className="market">market! </div>
       <div className="auction">auction</div>
-      <div className="patch">patch</div>
+      <Patch />
       <div className="cookoff">cookoff</div>
       <div className="city-hall">city hall</div>
       <div className="turn-order">turn order</div>
